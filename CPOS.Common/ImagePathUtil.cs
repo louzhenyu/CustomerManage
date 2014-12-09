2211586daae27502986adec23fa8850fa657ff44
baolin.han <baolin.han@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2014-12-09 10:31:35 +0000

    合并默认桩代码
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@36 67192a19-bcbd-5648-baee-99b7ca3a6e28

---
+++ CPOS.Common/ImagePathUtil.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+
+namespace JIT.CPOS.Common
+{
+    public class ImagePathUtil
+    {
+        /// <summary>
+        /// 获取图片缩略图路劲
+        /// </summary>
+        /// <param name="imagePath">原图路径</param>
+        /// <param name="size">缩略图大小</param>
+        /// <returns></returns>
+        public static string GetImagePathStr(string imagePath, string size)
+        {
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                string temp = imagePath.Substring(0, imagePath.Length - 4);
+                string extension = imagePath.Substring(imagePath.Length - 3);
+                string newPath = temp + "_" + size + "." + extension;
+                if (RemoteFileExists(newPath))
+                    return newPath;
+                else
+                    return imagePath;
+            }
+            else
+                return imagePath;
+        }
+        /// <summary>
+        /// 判断远程文件是否存在
+        /// </summary>
+        /// <param name="fileUrl"></param>
+        /// <returns></returns>
+        private static bool RemoteFileExists(string fileUrl)
+        {
+            bool result = false;//判断结果
+
+            WebResponse response = null;
+            try
+            {
+                WebRequest req = WebRequest.Create(fileUrl);
+
+                response = req.GetResponse();
+
+                result = response == null ? false : true;
+
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+
+            return result;
+        }
+    }
+}
