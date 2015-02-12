08fcc562945677392264b21753f4f9f2dc66d89f
baolin.han <baolin.han@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2015-02-12 06:47:35 +0000

    合并了修改版本号371 从 branches/Code: navList和categoryEntrance为空时，设为null ........
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@373 67192a19-bcbd-5648-baee-99b7ca3a6e28

--- CPOS.BS.Web/Module/AppConfig/Handler/HomePageHandler.ashx.cs
+++ CPOS.BS.Web/Module/AppConfig/Handler/HomePageHandler.ashx.cs
@@ -386,11 +386,19 @@ namespace JIT.CPOS.BS.Web.Module.AppConfig.Handler
                     {
                         content.categoryEntrance = lc.OrderByDescending(p => p.groupId).ToList()[0]; ;
                     }
+                    else {
+                        content.categoryEntrance = null ;
+                    }
+
                     // 过滤分类集合，把ModelTypeID=4的取出来(导航部门)
                     List<CategoryEntity> lc4 = content.categoryList.Where(p => p.modelTypeId == 4).ToList();
                     if (lc4 != null && lc4.Count != 0)
                     {
                         content.navList = lc4.OrderByDescending(p => p.groupId).ToList()[0]; ;//(获取唯一的)
+                    }
+                    else
+                    {
+                        content.navList = null;
                     }
                     //过滤分类集合，把ModelTypeID<>8的和不等于8的取出来
                     content.categoryList = content.categoryList.Where(p => p.modelTypeId != 8).Where(p => p.modelTypeId != 4).ToList();
