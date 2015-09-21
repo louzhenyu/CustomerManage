88872a9734479b77939590f54ea2afd412e6a154
song.zhou <song.zhou@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2015-09-21 07:44:14 +0000

    合并了修改版本号2519 从 branches/Code: 产品优化 修改返现 ........
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@2520 67192a19-bcbd-5648-baee-99b7ca3a6e28

--- CPOS.BS.DataAccess/WXSalesPolicyRateDAO.cs
+++ CPOS.BS.DataAccess/WXSalesPolicyRateDAO.cs
@@ -57,7 +57,12 @@ namespace JIT.CPOS.BS.DataAccess
             //sbSQL.Append(string.Format("select (CardinalNumber+({1} * Coefficient)) as ReturnAmount,PushInfo,* From [WXSalesPolicyRate]  where 1=1 and IsDelete=0 and CustomerId='{0}' and AmountBegin<={1} and AmountEnd>={1} ;", pCustomerId,SalesAmount));
             //sbSQL.Append(string.Format(" select (CardinalNumber+({0} * Coefficient)) as ReturnAmount,PushInfo,* From [WXSalesPolicyRate]  where 1=1 and IsDelete=0 and AmountBegin<={0} and AmountEnd>={0} and ([CustomerId]='' or CustomerId is NULL) ", SalesAmount));
             //–¬µƒ∑µœ÷≈‰÷√CustomerBasicSetting
-            ds = this.SQLHelper.ExecuteDataset(CommandType.StoredProcedure, "Proc_CalculateReturnCash");
+            SqlParameter[] parameters = new SqlParameter[2] 
+            { 
+                new SqlParameter{ParameterName="@Amount",SqlDbType=SqlDbType.Decimal,Value=SalesAmount},
+                new SqlParameter{ParameterName="@CustomerId",SqlDbType=SqlDbType.NVarChar,Value=pCustomerId}
+            };
+            ds = this.SQLHelper.ExecuteDataset(CommandType.StoredProcedure, "Proc_CalculateReturnCash", parameters);
             return ds;
         }
 
