932a3b91837eb8e3ff7423ead69ded36fd63c9ca
baolin.han <baolin.han@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2015-11-16 12:09:37 +0000

    会员&订单：+包含代码
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@3472 67192a19-bcbd-5648-baee-99b7ca3a6e28

---
+++ CPOS.BS.BLL/MobileHomeBLL.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Reflection;
+using JIT.Utility;
+using JIT.Utility.ExtensionMethod;
+using JIT.Utility.DataAccess;
+using JIT.Utility.DataAccess.Query;
+using JIT.CPOS.BS.DataAccess;
+using JIT.CPOS.BS.Entity;
+using JIT.CPOS.BS.DataAccess.Base;
+
+namespace JIT.CPOS.BS.BLL
+{
+    public partial class MobileHomeBLL
+    {
+        /// <summary>
+        /// 更新商户下所有主页的状态为非激活状态
+        /// </summary>
+        /// <param name="pEntity"></param>
+        public void UpdateIsActivate(MobileHomeEntity pEntity)
+        {
+            _currentDAO.UpdateIsActivate(pEntity);
+        }
+       /// <summary>
+        /// 根据模板Id生成新实例数据
+       /// </summary>
+       /// <param name="strHomeId"></param>
+        public void CreateStoreDataFromTemplate(string strHomeId, string strTemplateId)
+        {
+            _currentDAO.CreateStoreDataFromTemplate(strHomeId, strTemplateId);
+        }
+    }
+}
