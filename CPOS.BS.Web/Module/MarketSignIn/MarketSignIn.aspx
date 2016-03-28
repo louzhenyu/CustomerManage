720bc686a1ac90a41886c20fdaeab2c5332b7130
baolin.han <baolin.han@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2016-03-28 13:11:09 +0000

    合并了修改版本号7316, 7319 从 branches/CodeTest: Merged revision(s) 7309, 7315 from branches/Code: 连锁掌柜后台-会议签到-生成永久二维码 ........ 连锁掌柜后台-会议签到-生成永久二维码 ........
    ........
    Merged revision(s) 7317-7318 from branches/Code:
    连锁掌柜后台-会议签到-生成永久二维码
    ........
    连锁掌柜后台-会议签到-生成永久二维码
    ........
    
    ........
    
    
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@7380 67192a19-bcbd-5648-baee-99b7ca3a6e28

---
+++ CPOS.BS.Web/Module/MarketSignIn/MarketSignIn.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Framework/MasterPage/light.Master"
+    AutoEventWireup="true" Inherits="JIT.CPOS.BS.Web.PageBase.JITPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <meta charset="UTF-8" />
+    <title>会议签到</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="allPage" id="section" data-js="js/marketSignIn.js">
+    	<div class="signinArea">
+        	<div class="itemBox">
+            	<a class="createSigninQR commonBtn w120" href="javascript:;">生成签到二维码</a>
+            	<!--显示后台接口返回二维码路径-->
+                <p class="signinQRImg">
+                	<img src=""/>
+                </p>
+            </div>
+            <div class="itemBox">
+            	<a class="downloadVipQR commonBtn w120" target="_blank" href="javascript:;">下载会员二维码</a>
+            </div>
+        </div>
+    	
+    </div>
+<script type="text/javascript" src="<%=StaticUrl+"/Module/static/js/lib/require.min.js"%>" defer async="true" data-main="<%=StaticUrl+"/module/commodity/js/main.js"%>"></script>
+</asp:Content>
---
+++ CPOS.BS.Web/Module/MarketSignIn/js/marketSignIn.js
@@ -0,0 +1,48 @@
+﻿define(['jquery','tools','template', 'kindeditor',"easyui"], function ($) {
+    var page = {
+        elem:{
+            $createSigninQR:$(".createSigninQR"),
+			$downloadVipQR:$(".downloadVipQR"),
+			$signinQRImg:$(".signinQRImg"),
+        },
+        //初始化参数
+        init: function () {
+			var that = this;			/*that.elem.$downloadVipQR.attr('onlick','javascript:window.location.href="'+location.host+'/ApplicationInterface/Module/Market/MarketNameApply/GenerateMarketEventQR.ashx"');*/
+			that.elem.$downloadVipQR.attr('href','/ApplicationInterface/Module/Market/MarketNameApply/GenerateMarketEventQR.ashx?type=Product&action=');
+            that.initEvent();
+        },
+        //初始化事件
+        initEvent:function(){
+			var that = this;
+			that.elem.$createSigninQR.bind('click',function(){
+				that.createSigninQR();
+			});
+        },
+        // 初始化页面数据
+        loadPageData:function(){
+
+        },
+        createSigninQR:function(){
+			var that = this;
+			$.util.ajax({
+				url: "/ApplicationInterface/Gateway.ashx",
+				data:{
+					action:"Market.CreateQRCode.DownloadQRCode",
+					"VipDCode":9
+				},
+				success: function (data) {
+					if (data.IsSuccess) {
+						that.elem.$signinQRImg.html('<img src="' + data.Data.imageUrl + '" />');
+
+					} else {
+						$.messager.alert("操作失败提示",data.Message);
+					}
+				},error: function(data) {
+					$.messager.alert("操作失败提示",data.Message);
+				}
+			});
+		}
+
+    };
+    page.init();
+}) ;
