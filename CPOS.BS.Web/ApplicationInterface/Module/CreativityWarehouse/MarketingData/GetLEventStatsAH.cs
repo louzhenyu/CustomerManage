9e61ae703f1eeb8fc537538b1f76ef6fc3a9d28e
baolin.han <baolin.han@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2016-04-16 09:15:20 +0000

    创意仓库：手动合并
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@8189 67192a19-bcbd-5648-baee-99b7ca3a6e28

---
+++ CPOS.BS.Web/ApplicationInterface/Module/CreativityWarehouse/MarketingData/GetLEventStatsAH.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JIT.CPOS.BS.Web.ApplicationInterface.Base;
+using JIT.CPOS.DTO.Module.CreativityWarehouse.MarketingData.Request;
+using JIT.CPOS.DTO.Module.CreativityWarehouse.MarketingData.Response;
+using JIT.CPOS.BS.BLL;
+using JIT.CPOS.BS.Entity;
+using JIT.CPOS.DTO.Base;
+using JIT.Utility.Log;
+using JIT.Utility.ExtensionMethod;
+using JIT.Utility.DataAccess.Query;
+using JIT.Utility.Notification;
+using JIT.CPOS.BS.Web.Session;
+using System.Data;
+namespace JIT.CPOS.BS.Web.ApplicationInterface.Module.CreativityWarehouse.MarketingData
+{
+    public class GetLEventStatsAH : BaseActionHandler<CTWEventRP, GetLEventStatsRD>
+    {
+        protected override GetLEventStatsRD ProcessRequest(APIRequest<CTWEventRP> pRequest)
+        {
+            var rd = new GetLEventStatsRD();
+
+            var para = pRequest.Parameters;
+            var loggingSessionInfo = new SessionManager().CurrentUserLoginInfo;
+
+            T_CTW_LEventBLL bllLEvent = new T_CTW_LEventBLL(loggingSessionInfo);
+            DataSet ds = bllLEvent.GetCTW_LEventStats(para.CTWEventId);
+            if(ds!=null && ds.Tables.Count>0 && ds.Tables[0].Rows.Count>0)
+            {
+                rd.LeventsStats = DataTableToObject.ConvertToObject<LeventsStats>(ds.Tables[0].Rows[0]);
+            }
+            return rd;
+        }
+    }
+}
---
+++ CPOS.BS.Web/ApplicationInterface/Module/CreativityWarehouse/MarketingData/GetPanicbuyingEventRankingStatsAH.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JIT.CPOS.BS.Web.ApplicationInterface.Base;
+using JIT.CPOS.DTO.Module.CreativityWarehouse.MarketingData.Request;
+using JIT.CPOS.DTO.Module.CreativityWarehouse.MarketingData.Response;
+using JIT.CPOS.BS.BLL;
+using JIT.CPOS.BS.Entity;
+using JIT.CPOS.DTO.Base;
+using JIT.Utility.Log;
+using JIT.Utility.ExtensionMethod;
+using JIT.Utility.DataAccess.Query;
+using JIT.Utility.Notification;
+using JIT.CPOS.BS.Web.Session;
+using System.Data;
+namespace JIT.CPOS.BS.Web.ApplicationInterface.Module.CreativityWarehouse.MarketingData
+{
+    public class GetPanicbuyingEventRankingStatsAH : BaseActionHandler<CTWEventRP, GetPanicbuyingEventRankingStatsRD>
+    {
+        protected override GetPanicbuyingEventRankingStatsRD ProcessRequest(APIRequest<CTWEventRP> pRequest)
+        {
+            var rd = new GetPanicbuyingEventRankingStatsRD();
+
+            var para = pRequest.Parameters;
+            var loggingSessionInfo = new SessionManager().CurrentUserLoginInfo;
+
+            T_CTW_LEventBLL bllLEvent = new T_CTW_LEventBLL(loggingSessionInfo);
+            DataSet ds = bllLEvent.GetCTW_PanicbuyingEventRankingStats(para.CTWEventId);
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                if(ds.Tables[0].Rows.Count>0)
+                {
+                    rd.OrderMoneyRankList = DataTableToObject.ConvertToList<OrderMoneyRank>(ds.Tables[0]);
+
+                }
+                if (ds.Tables[1].Rows.Count > 0)
+                {
+                    rd.OrderCountRankList = DataTableToObject.ConvertToList<OrderCountRank>(ds.Tables[1]);
+                }
+            }
+            return rd;
+        }
+    }
+}
