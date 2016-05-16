c7c43976d0d2353182ec7b3ec92ee0938c3bbfd0
baolin.han <baolin.han@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2016-05-16 06:01:36 +0000

    砍价-包含到svn
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@9787 67192a19-bcbd-5648-baee-99b7ca3a6e28

---
+++ CPOS.DTO/Module/Event/Bargain/Request/EventListRP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JIT.CPOS.DTO.Base;
+using JIT.CPOS.BS.Entity;
+
+namespace JIT.CPOS.DTO.Module.Event.Bargain.Request
+{
+    public class EventListRP : IAPIRequestParameter
+    {
+        public int EventTypeId { get; set; } // 活动类型 4-砍价
+
+        public void Validate() { }
+    }
+}
---
+++ CPOS.DTO/Module/Event/Bargain/Response/EventListRD.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JIT.CPOS.DTO.Base;
+using JIT.CPOS.BS.Entity;
+
+namespace JIT.CPOS.DTO.Module.Event.Bargain.Response
+{
+    public class EventListRD : IAPIResponseData
+    {
+        public List<EventInfo> EventList { get; set; }
+
+        public int IsAllEnd { get; set; } //1-全都为已结束的活动
+    }
+
+    public class EventInfo
+    {
+        public string EventId { get; set; } // 活动Id
+
+        public string BeginTime { get; set; } //开始时间
+
+        public string EndTime { get; set; } //结束时间
+
+        public long Seconds { get; set; } //现在到结束时间
+        public int Status { get; set; } //状态 0-进行中 1-即将开始 2-已结束
+
+        public List<EventItem> EventItemList { get; set; }
+    }
+
+    public class EventItem
+    {
+
+        public string ItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string ImageUrlThumb { get; set; }
+
+        public string ImageUrlMiddle { get; set; }
+
+        public string ImageUrlBig { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public int Qty { get; set; }
+
+        public int PromotePersonCount { get; set; }
+    }
+}
