7237a94b4dee9f797e8b1345dd0df5037f8bdd04
baolin.han <baolin.han@67192a19-bcbd-5648-baee-99b7ca3a6e28>
2016-05-27 10:27:45 +0000

    redis 优惠券相关 byjianxian
    git-svn-id: https://10.133.192.229/svn/CustomerManage/trunk/Code@10578 67192a19-bcbd-5648-baee-99b7ca3a6e28

---
+++ CPOS.BS.BLL/RedisOperationBLL/Connection/RedisConnectionBLL.cs
@@ -0,0 +1,86 @@
+﻿using JIT.CPOS.BS.Entity;
+using RedisOpenAPIClient;
+using RedisOpenAPIClient.Models;
+using RedisOpenAPIClient.Models.CC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JIT.CPOS.BS.BLL.RedisOperationBLL.Connection
+{
+    /// <summary>
+    /// 商户数据库链接
+    /// </summary>
+    public class RedisConnectionBLL
+    {
+        /// <summary>
+        /// 设置缓存
+        /// </summary>
+        public void SetConnection(string _customerID, string _ConnectionStr, string _Customer_Name, string _Customer_Code)
+        {
+
+            try
+            {
+                RedisOpenAPI.Instance.CCConnection().SetConnection(new CC_Connection
+                {
+                    CustomerID = _customerID,
+                    ConnectionStr = _ConnectionStr,
+                    Customer_Name = _Customer_Name,
+                    Customer_Code = _Customer_Code
+                });
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("设置缓存失败!" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 获取缓存(商户数据库链接)
+        /// </summary>
+        public CC_Connection GetConnection(string customerID)
+        {
+            try
+            {
+                var response = RedisOpenAPI.Instance.CCConnection().GetConnection(new CC_Connection
+                {
+                    CustomerID = customerID,
+                });
+                if (response.Code == ResponseCode.Success)
+                {
+                    return response.Result;
+                }
+                else
+                {
+                    return new CC_Connection();
+                }
+            }
+            catch
+            {
+                return new CC_Connection();
+            }
+        }
+
+        /// <summary>
+        /// 删除缓存
+        /// </summary>
+        public void DelConnection(string customerID)
+        {
+            try
+            {
+                RedisOpenAPI.Instance.CCConnection().DelConnection(new CC_Connection
+                {
+                    CustomerID = customerID
+                });
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("删除缓存失败!" + ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ CPOS.BS.BLL/RedisOperationBLL/Connection/SetConnectionJobBLL.cs
@@ -0,0 +1,69 @@
+﻿
+using JIT.CPOS.BS.Entity;
+using JIT.Utility.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+
+namespace JIT.CPOS.BS.BLL.RedisOperationBLL.Connection
+{
+    public class SetConnectionJobBLL
+    {
+        /// <summary>
+        /// 所有商户
+        /// </summary>
+        private Dictionary<string, string> _CustomerIDList
+        { get; set; }
+      
+   
+
+        /// <summary>
+        /// 种植 角色菜单 缓存
+        /// </summary>
+        public void AutoSetRoleMenuCache()
+        {
+            _CustomerIDList = CustomerBLL.Instance.GetCustomerList();//这里的Instance使用了单例的模式
+            foreach (var customer in _CustomerIDList)
+            {
+                string name = GetCustomerName(customer.Key);
+                string code = GetCustomerCode(customer.Key);
+                //string conn = customer.Value;//取链接字符串             
+                new RedisConnectionBLL().SetConnection(customer.Key, customer.Value, name, code);//key和value分别代表商户的id和链接字符串
+              
+            }
+        }
+
+        #region GetCustomerConn
+        /// <summary>
+        /// 获取商户编码
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public static string GetCustomerCode(string customerId)
+        {
+            string sql = string.Format(
+                "select a.customer_code from t_customer a where a.customer_id='{0}' ",
+                customerId);
+            string conn = ConfigurationManager.AppSettings["Conn_ap"];
+            DefaultSQLHelper sqlHelper = new DefaultSQLHelper(conn);
+            var result = sqlHelper.ExecuteScalar(sql);
+            return result == null || result == DBNull.Value ? string.Empty : result.ToString();
+        }
+
+        public static string GetCustomerName(string customerId)
+        {
+            string sql = string.Format(
+                "select a.customer_name from t_customer a where a.customer_id='{0}' ",
+                customerId);
+            string conn = ConfigurationManager.AppSettings["Conn_ap"];
+            DefaultSQLHelper sqlHelper = new DefaultSQLHelper(conn);
+            var result = sqlHelper.ExecuteScalar(sql);
+            return result == null || result == DBNull.Value ? string.Empty : result.ToString();
+        }
+        #endregion
+
+
+    }
+}
